51ab07cef3b1cfe126afa73ffb6693a168619d75
Kamil1developer <igologramma@mail.ru>
2026-05-04 15:29:29 +0300

    поменял record на обычный класс

--- src/main/java/dto/view/CurrentMatchViewDto.java
+++ src/main/java/dto/view/CurrentMatchViewDto.java
@@ -1,10 +1,14 @@
 package dto.view;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import scores.Scores;
 
-public record CurrentMatchViewDto(
-        String firstPlayerName,
-        String secondPlayerName,
-        Scores firstPlayerScores,
-        Scores secondPlayerScores
-        ) {}
+@AllArgsConstructor
+@Getter
+public class CurrentMatchViewDto {
+    private final String firstPlayerName;
+    private final String secondPlayerName;
+    private final Scores firstPlayerScores;
+    private final Scores secondPlayerScore;
+}

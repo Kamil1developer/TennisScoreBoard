a624bca9800b9d558c988d448d16925b7bf8b3b4
Kamil1developer <igologramma@mail.ru>
2026-05-07 23:33:19 +0300

    "0"

--- src/main/java/service/NewMatchService.java
+++ src/main/java/service/NewMatchService.java
@@ -52,8 +52,8 @@ public class NewMatchService {
         CurrentMatch currentMatch = new CurrentMatch(
                 firstPlayer.getId(),
                 secondPlayer.getId(),
-                new Score(0, 0, 0,false),
-                new Score(0, 0, 0,false)
+                new Score(0, 0, "0",false),
+                new Score(0, 0, "0",false)
         );
 
         UUID uuid = UUID.randomUUID();
